31a4cae42384d1f99d6043bc71106bbad04a135f
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-11-12 14:08:41 +0300

    Removed imageMinify cause it takes a lot of time

--- gulp/imageMinify.js
+++ gulp/imageMinify.js
@@ -3,19 +3,19 @@ const imagemin = require( 'gulp-imagemin' )
 
 module.exports = function imageMinify() {
   return gulp.src( 'src/img/**/*.{gif,png,jpg,jpeg,svg,webp}' )
-    .pipe( imagemin( [
-      imagemin.gifsicle( { interlaced: true } ),
-      imagemin.mozjpeg( {
-        quality: 75,
-        progressive: true
-      } ),
-      imagemin.optipng( { optimizationLevel: 5 } ),
-      imagemin.svgo( {
-        plugins: [
-          { removeViewBox: true },
-          { cleanupIDs: false }
-        ]
-      } )
-    ] ) )
+    // .pipe( imagemin( [
+    //   imagemin.gifsicle( { interlaced: true } ),
+    //   imagemin.mozjpeg( {
+    //     quality: 75,
+    //     progressive: true
+    //   } ),
+    //   imagemin.optipng( { optimizationLevel: 5 } ),
+    //   imagemin.svgo( {
+    //     plugins: [
+    //       { removeViewBox: true },
+    //       { cleanupIDs: false }
+    //     ]
+    //   } )
+    // ] ) )
     .pipe( gulp.dest( 'build/img' ) )
 }
