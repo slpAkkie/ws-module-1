79cb36f9b5cde76ec4d4334e80b34df7f04adace
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-12-03 21:58:50 +0300

    Added my own Lazy-Load

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,41 @@
 
 $( document ).ready( function () {
 
+  /** Плавное появление карточек принципа накопления */
+  $( document ).scroll( showPrincipleCards );
+  showPrincipleCards();
+
   /** По нажатию на кнопку-бургер переключить видимость меню */
   $( '#js-burger' ).click( toggleNavMenu );
 
 } );
+
+
+
+/**
+ *
+ * =========================
+ * Независимые функции
+ * =========================
+ *
+ */
+
+
+
+/**
+ * Плавное появление карточек принципа накопления
+ * =========================
+ */
+function showPrincipleCards() {
+  $( '.lazy-load' ).each( function () {
+    if (
+      ( window.scrollY <= this.offsetTop + this.clientHeight )
+      &&
+      ( window.scrollY + window.innerHeight >= this.offsetTop )
+    ) $( this ).attr( 'data-shown', 'true' );
+    else $( this ).attr( 'data-shown', 'false' );
+  } );
+}
 
 
 
