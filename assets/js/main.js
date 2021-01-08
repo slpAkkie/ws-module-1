c1ecc74a9a0fdfe7806cfa1c33c25c6d507ec08c
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-01-08 11:32:06 +0300

    Return code that I accidentally deleted
    And also fix some bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,61 @@
-ScrollFect.appearance( '.scrollfect', {
-    animation: ScrollFect.animationStore.appearanceFade,
-    duration: 0.75,
-    gap: 100,
-    params: {
-        minScale: 0.85
-    }
-} );
+document.addEventListener( 'DOMContentLoaded', () => {
 
-ScrollFect.appearance( '.scrollfect-once', {
-    animation: ScrollFect.animationStore.appearanceFade,
-    duration: 0.75,
-    gap: { top: 120, bottom: 80 },
-    once: true,
-    onVisible: false,
-    params: {
-        minScale: 0.85
-    }
-} );
+    ScrollFect.appearance( '.scrollfect', {
+        animation: ScrollFect.animationStore.appearanceFade,
+        duration: 0.75,
+        gap: 100,
+        params: {
+            minScale: 0.85
+        }
+    } );
 
-ScrollFect.appearance( 'nav.header-nav.top', {
-    animation: ScrollFect.animationStore.appearanceSlideTop,
-    duration: 0.75,
-    once: true,
-    onVisible: false,
-    params: {
-        offsetTop: 25
+    ScrollFect.appearance( '.scrollfect-once', {
+        animation: ScrollFect.animationStore.appearanceFade,
+        duration: 0.75,
+        gap: { top: 120, bottom: 80 },
+        once: true,
+        onVisible: false,
+        params: {
+            minScale: 0.85
+        }
+    } );
+
+    ScrollFect.appearance( 'nav.header-nav.top', {
+        animation: ScrollFect.animationStore.appearanceSlideTop,
+        duration: 0.75,
+        once: true,
+        onVisible: false,
+        params: {
+            offsetTop: 25
+        }
+    } );
+
+    $( window ).scroll( navChange );
+
+    $( '#js-burger' ).click( function () {
+        $( '.header-nav__menu' ).toggleClass( 'shown' );
+
+        $( 'body' ).toggleClass( 'scroll-fix' );
+    } );
+
+    $( '.seat-block' ).click( function () {
+        if ( !$( this ).hasClass( 'chosen' ) ) $( '.seat-block' ).each( function () {
+            $( this ).removeClass( 'chosen' );
+        } );
+
+        $( this ).toggleClass( 'chosen' );
+    } );
+
+    $( '.js-booking' ).click( () => {
+        location.href = '/booking.html';
+    } );
+
+
+    function navChange() {
+        if ( window.pageYOffset > 70 )
+            $( 'header.header nav.header-nav' ).removeClass( 'top' );
+        else
+            $( 'header.header nav.header-nav' ).addClass( 'top' );
     }
+
 } );
